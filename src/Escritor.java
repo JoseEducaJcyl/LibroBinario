09196e68e660a7dabd8a6bb2f5f15153de00fec2
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-02-10 12:09:58 +0100

    Initial commit

---
+++ src/Escritor.java
@@ -0,0 +1,15 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class GuardarLibro {
+    public void guardarLibro(List<Libro> libros, String ruta) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
+            oos.writeObject(libros);
+            System.out.println("Objeto Libro serializado y guardado en libros.cosa");
+        } catch (IOException e) {
+            System.out.println("Error al guardar el objeto: " + e.getMessage());
+        }
+    }
+}
