144560735020bec93b4a12d374b42b082ea492fa
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:30:03 +0100

    Comentarios agregados

--- src/Escritor.java
+++ src/Escritor.java
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class Escritor {
+    //Metodo guardarLibro, se le pasa una List de libros y un String con la ruta y nombre del archivo 
     public void guardarLibro(List<Libro> libros, String ruta) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
             oos.writeObject(libros);
